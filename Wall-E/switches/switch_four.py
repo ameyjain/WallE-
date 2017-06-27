9676a81ddbc535ef99e842bf65ce3c8c30aec4b2
Amey Home <AmeyJain@Shubhis-MacBook-Pro.local>
2017-06-27 12:12:41 -0400

    initial commit

---
+++ Wall-E/switches/switch_four.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+pin = 23
+state = GPIO.input(pin)
+GPIO.setup(pin, GPIO.OUT)
+
+if (!state):
+    GPIO.output(pin, GPIO.HIGH)
+    print ("ON")
+else:
+    GPIO.output(pin, GPIO.LOW)
+    print ("OFF")
+
