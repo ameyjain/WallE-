f74e6adceb4ff3cfa270851aed90d3cadc1c6478
Amey Home <AmeyJain@Shubhis-MacBook-Pro.local>
2017-10-29 18:00:22 -0400

    redesign footer

--- Website/src/components/Footers/Footer.js
+++ Website/src/components/Footers/Footer.js
@@ -5,30 +5,43 @@ export default class Footer extends Component {
 
   render() {
       return (
-        <div>
         <section id="footer">
           <div className="inner">
-            <form method="post" action="#">
-              <div className="field">
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" />
-              </div>
-              <ul className="actions">
-                <li><input type="submit" value="Subscribe Now" /></li>
-              </ul>
-            </form>
+          	<section className="content">
+          		<h4><u>FAQ</u></h4>
+              <h4><u>CONTACT</u></h4>
+              <h4><u>SHOP</u></h4>
+          	</section>
 
-            <ul className="footer-social-icons social-icons">
-                <li><a href="https://facebook.com/amey14" className="social-icon"> <i className="fa fa-facebook"></i></a></li>
-                <li><a href="https://twitter.com/geeksOnly" className="social-icon"> <i className="fa fa-twitter"></i></a></li>
-                <li><a href="https://instagram.com/ameyjain" className="social-icon"> <i className="fa fa-instagram"></i></a></li>
-            </ul>
-            <ul className="copyright">
-              <li>&copy; Wall-e Inc. All rights reserved.</li>
-            </ul>
+          	<section className="middle">
+          		<h4>ABOUT US</h4>
+          		<p>Wall-e believes in bringing products that make your environment smart by bringing the best technology together and at an affordable rate. We make smart things smarter.</p>
+          	</section>
+
+          	<aside className="sidebar">
+          		<h4>NEW RELEASE</h4>
+          		<p>The first ever smart speaker that can become a google home and alexa at your command. Giving you the best of both worlds.</p>
+
+              <form method="post" action="#">
+                <div className="field">
+                  <input type="email" name="email" id="email" value="Email" />
+                </div>
+                <ul className="actions vertical">
+                <li><a href="#first" className="button big wide smooth-scroll-middle special">Subscribe Now</a></li>
+                </ul>
+              </form>
+          	</aside>
+
+            <div className="footer-social-icons">
+                <ul className="social-icons">
+                    <li><a href="" className="social-icon"> <i className="fa fa-facebook"></i></a></li>
+                    <li><a href="" className="social-icon"> <i className="fa fa-twitter"></i></a></li>
+                    <li><a href="" className="social-icon"> <i className="fa fa-instagram"></i></a></li>
+                </ul>
+                <p>WALL-E INC.</p>
+            </div>
           </div>
         </section>
-        </div>
-      )
+       )
   }
 }
--- Website/src/components/Header/Header.js
+++ Website/src/components/Header/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
   render() {
       return (
         <section id="banner">
-          <div className="inner">
+            <div className="inner">
             <div className="logo"><span className="icon fa-diamond"></span></div>
             <h2>Wall-e Inc.</h2>
             <p>Your home just got twice as smart.</p>
