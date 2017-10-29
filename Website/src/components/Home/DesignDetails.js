809e40348f739476ef80584b293f96ae4b592788
Admin <aj.jain1992@gmail.com>
2017-10-29 18:41:35 -0400

    Add header to design details section

--- Website/src/components/Home/DesignDetails.js
+++ Website/src/components/Home/DesignDetails.js
@@ -6,6 +6,12 @@ export default class DesignDetails extends Component {
   render() {
       return (
         <div className="whiteSection align-center">
+        <div className="designHeading" id="line1">
+          <span className="DesignHeading">MINIMAL & SMART</span>
+        </div>
+        <div className="designHeading">
+          <span className="DesignHeading">DESIGN</span>
+        </div>
         <div id="imageDiv1">
           <img src="https://static.wixstatic.com/media/42dbaa_5a33625cc7c04c7b933fdcb7008322a0.png/v1/fill/w_531,h_611,al_c,usm_0.66_1.00_0.01/42dbaa_5a33625cc7c04c7b933fdcb7008322a0.png"/>
         </div>
