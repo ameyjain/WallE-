1bbd05f14a7f0ccbde53eb9dd61c2bd99314ad37
Admin <aj.jain1992@gmail.com>
2017-11-01 07:13:20 -0400

    Fix banner padding, fix info grid color

--- Website/src/components/Home/InfoGrid.js
+++ Website/src/components/Home/InfoGrid.js
@@ -9,27 +9,27 @@ export default class InfoGrid extends Component {
           <div className="inner">
             <div className="items style1 medium onscroll-fade-in">
               <section>
-                <span className="icon style2 major fa-diamond"></span>
+                <span className="icon style2 major fa-wifi"></span>
                 <h3>WIRELESS</h3>
                 <p>Just connect the speaker to same wifi as your smart phone or tablet and you are good to go.</p>
               </section>
               <section>
-                <span className="icon style2 major fa-save"></span>
+                <span className="icon style2 major fa-microphone"></span>
                 <h3>POWERFUL MIC</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
               </section>
               <section>
-                <span className="icon style2 major fa-bar-chart"></span>
+                <span className="icon style2 major fa-mobile"></span>
                 <h3>EASY CONTROL</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
               </section>
               <section>
-                <span className="icon style2 major fa-wifi"></span>
+                <span className="icon style2 major fa-cog"></span>
                 <h3>SIMPLE SETUP</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
               </section>
               <section>
-                <span className="icon style2 major fa-cog"></span>
+                <span className="icon style2 major fa-cube"></span>
                 <h3>SUPIRIOR DESIGN</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
               </section>
