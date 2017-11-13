3f4c863d0e0e33569549b61638304e130ea6651d
Admin <aj.jain1992@gmail.com>
2017-11-13 15:48:27 -0500

    update gitignore

---
+++ Website/src/pages/Static/checkout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { CheckoutContent } from '../../components/Checkout';
+
+export default class Checkout extends Component {
+  render() {
+    return (
+      <div>
+        <CheckoutContent/>
+      </div>
+      );
+  }
+}
---
+++ Website/src/pages/Static/productDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { ProductDetailsContent } from '../../components/ProductDetails';
+
+export default class ProductDetails extends Component {
+  render() {
+    return (
+      <div>
+        <ProductDetailsContent />
+      </div>
+      );
+  }
+}
