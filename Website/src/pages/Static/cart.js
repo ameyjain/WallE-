03877ef17ba9fbc7bf49945dead9a036287afb1b
Amey Home <AmeyJain@Shubhis-MacBook-Pro.local>
2017-11-13 15:50:42 -0500

    Adding static cart.js

---
+++ Website/src/pages/Static/cart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { CartContent } from '../../components/Cart';
+
+export default class Cart extends Component {
+  render() {
+    return (
+      <div>
+        <CartContent/>
+      </div>
+    );
+  }
+}
